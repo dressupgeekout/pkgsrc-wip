b54bb2d00e1e87383ab3288f0fb92d9620f86c58
Charlotte Koch <cfkoch@edgebsd.org>
2019-08-17 01:43:38 -0700

    guess we need this

--- stratagus-git/buildlink3.mk
+++ stratagus-git/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	stratagus
 .if !defined(STRATAGUS_BUILDLINK3_MK)
 STRATAGUS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.stratagus+=	stratagus-2.4.*
-BUILDLINK_PKGSRCDIR.stratagus?=		../../wip/stratagus
+BUILDLINK_API_DEPENDS.stratagus+=	stratagus-*
+BUILDLINK_PKGSRCDIR.stratagus?=		../../wip/stratagus-git
 
 .endif # STRATAGUS_BUILDLINK3_MK
 
