2de75b7ea0890283042b40b3c87c3f6b7991b01e
Charlotte Koch <charlotte@NetBSD.org>
2022-05-18 17:50:52 -0700

    opencolorio 2 new bl3

--- opencolorio/buildlink3.mk
+++ opencolorio/buildlink3.mk
@@ -5,8 +5,8 @@ BUILDLINK_TREE+=	OpenColorIO
 .if !defined(OPENCOLORIO_BUILDLINK3_MK)
 OPENCOLORIO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.OpenColorIO+=	OpenColorIO>=1.0.9
-BUILDLINK_PKGSRCDIR.OpenColorIO?=	../../graphics/opencolorio
+BUILDLINK_API_DEPENDS.OpenColorIO+=	OpenColorIO>=2.1.1
+BUILDLINK_PKGSRCDIR.OpenColorIO?=	../../wip/opencolorio
 
 .include "../../graphics/lcms2/buildlink3.mk"
 .include "../../textproc/tinyxml/buildlink3.mk"
