3ceaff31f20cfd315c28794ed631eaaa5483f4d9
Charlotte Koch <charlotte@NetBSD.org>
2022-06-02 22:13:30 -0700

    mygui-ogre: Fix mistakes in bl3

--- mygui-ogre/buildlink3.mk
+++ mygui-ogre/buildlink3.mk
@@ -6,9 +6,10 @@ BUILDLINK_TREE+=	mygui-ogre
 MYGUI_OGRE_BUILDLINK3_MK:=
 
 BUILDLINK_API_DEPENDS.mygui-ogre+=	mygui-ogre>=3.2.2
-BUILDLINK_ABI_DEPENDS.mygui-ogre?=	mygui-ogre>=3.2.3.20190707nb5
+BUILDLINK_ABI_DEPENDS.mygui-ogre?=	mygui-ogre>=3.2.3.20190707
 BUILDLINK_PKGSRCDIR.mygui-ogre?=	../../wip/mygui-ogre
 
+.include "../../devel/ogre/buildlink3.mk"
 .include "../../devel/SDL2/buildlink3.mk"
 .include "../../graphics/freetype2/buildlink3.mk"
 .include "../../graphics/MesaLib/buildlink3.mk"
