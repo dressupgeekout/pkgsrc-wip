c5621ac4b1bc30bcb9a6b399aef6e68dcbe8ce44
Charlotte Koch <charlotte@NetBSD.org>
2023-06-29 01:00:05 -0700

    ppsspp: This builds, but we can't play any games

--- ppsspp/patches/patch-ppsspp_config.h
+++ ppsspp/patches/patch-ppsspp_config.h
@@ -1,3 +1,7 @@
+$NetBSD$
+
+NetBSD can benefit from the macros which allow PPSSPP to build on OpenBSD.
+
 --- ppsspp_config.h.orig	2023-06-28 20:56:35.580340514 -0700
 +++ ppsspp_config.h	2023-06-28 20:56:48.149019856 -0700
 @@ -104,7 +104,7 @@
